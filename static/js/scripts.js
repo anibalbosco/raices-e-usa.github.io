68b52f5d6b060b0ca008be30058bd6d926bde7ad
anibalbosco <86413500+anibalbosco@users.noreply.github.com>
2024-11-26 23:45:39 -0500

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'comites', 'recursos', 'premios']
+const section_names = ['home', 'comites', 'recursos', 'miembros', 'premios']
 
 
 window.addEventListener('DOMContentLoaded', event => {
